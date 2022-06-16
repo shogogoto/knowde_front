0b219aa1e355c221030ac5d4d8e9bf9a2b820a2a
Shogo Goto <gotoadmn0605@gmail.com>
2022-06-16 09:13:15 +0900

    second

--- knowde_front/src/App.tsx
+++ knowde_front/src/App.tsx
@@ -1,27 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import LoginForm from "./features/Login";
+// import Popup from "./components/Popup";
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Contact() {
+  return <h2>Contact</h2>;
+}
+
+function NotFound() {
+  return <h2>Not Found</h2>;
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        {/*
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        */}
-      </header>
-    </div>
+  <BrowserRouter>
+    <Header />
+    <h1>Hello React Router</h1>
+      <p><Link to="/">Home</Link></p>
+      <p><Link to="/about">About</Link></p>
+      <p><Link to="/contact">Contact</Link></p>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
